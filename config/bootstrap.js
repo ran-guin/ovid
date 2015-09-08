a18a163bf764a16126c2b92f6693345e75a6bbd0
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-09-08 11:50:08 -0700

    dynamically set enum in SQL based upon models

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,46 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+
+	var models = Object.keys(sails.models);
+
+	console.log("Auto-correcting ENUM fields for models:\n* " + models.join("\n* ") );
+
+	var errors = [];
+	var added_enum = 0;
+	for (var i=0; i< models.length; i++) {
+
+  		var Model = sails.models[models[i]];
+  		var attributes = Object.keys(Model.attributes);
+
+  		// console.log("*** " + models[i] + " ***");
+  		for (var j=0; j<attributes.length; j++) {
+			var att = attributes[j];
+
+			var Atype = Model.attributes[att].type;
+			var Aenum =  Model.attributes[att].enum;
+			
+			// console.log(att + " : " + Atype + " " + Aenum);
+			if (Aenum) {
+				var command = " ALTER TABLE " + models[i] + " MODIFY " + att + " ENUM('" + Aenum.join("','") + "')";
+				console.log(command); 
+
+		  		Record.query(command, function (err, result) {
+		  			if (err) {
+						console.log("ERROR:" + err);
+						cb(err);
+		     		}
+		  		});
+
+		  		added_enum++;
+		  	}
+		  }
+	}
+
+	if (errors.length) { cb( " Errors: " + errors.join("; ")) }
+	else { 
+		console.log("Updated " + added_enum + " ENUM fields ");
+		cb();
+	}
+
 };
