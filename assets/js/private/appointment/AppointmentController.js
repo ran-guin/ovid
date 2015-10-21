4ecb936947d2d80a64b95fd02f0e07daab40d597
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-10-21 16:35:12 -0700

    centralize initialization of angular variables from config input

--- assets/js/private/appointment/AppointmentController.js
+++ assets/js/private/appointment/AppointmentController.js
@@ -96,54 +96,19 @@ app.controller('AppointmentController',
       $scope.$parent[label] = $scope[attribute]['label'];
     }
 
-    $scope.initialize = function( config ) {
+    $scope.loadRecommendations = function (data) {
+        console.log('load recommendations');
 
-        console.log("Appointment init: " + JSON.stringify(config));
-        
-        if (config && config['User']) { 
-            console.log("loaded user attributes");
-            $scope.$parent.user = config['User'];
-        }
-        if (config && config['clinic']) { 
-            console.log("loaded clinic attributes in Appointment init");
-            $scope.$parent.clinic = config['clinic'];
-        }
-        if (config && config['patient']) { 
-            console.log("loaded patient attributes");
-            $scope.$parent.patient = config['patient'];
-        }
- 
-        if (config && config['clinic'] && config['clinic']['appointments']) {
-            console.log("Start with " + $scope.items.length);
-            $scope.$parent.items = config['clinic']['appointments'];
-            console.log("loaded " + $scope.items.length + " clinic appointments");
-        }    
- 
-        $scope.$parent.highlightBackground = "background-color:#9C9;";
-        var highlight_element = document.getElementById('clinicTab');
-        if (highlight_element) {
-            highlight_element.style=($scope.highlightBackground)
-        }
-
-        $scope.setup(config);
-
-        $scope.$parent.initialize(config);
-
-        if ($scope.recordId) { $scope.loadRecord($scope.recordId) }
-        else {
-            console.log("Initialize " + $scope.statusField + '=' + $scope.statusDefault);
-            if ($scope.statusField ) {
-                if ($scope[$scope.statusField] === undefined) {
-                    $scope.$parent[$scope.statusField] = $scope.statusDefault;
-                    console.log('set default status to' + $scope.recordStatus);
-                }
-                Nto1Factory.setClasses($scope.statusOptions, $scope[$scope.statusField]);  
-            }
-        }
-
-        $scope.$parent.ac_options = JSON.stringify($scope.Autocomplete);
-
-        $scope.$parent.manualSet = []; /* 'Request_Notes'];  /* manually reset */
+        $scope.$parent['recommendations'] = data;
+         console.log("got recommendations: " + JSON.stringify(data));
+  
+        /*
+        $http.get("/recommendation")
+        .success ( function (response) {
+            $scope.$parent['recommendation'] = response;
+            console.log("got recommendations: " + JSON.stringify(response));
+        });
+        */
     }
 
     $scope.loadPatientHistory = function (patient_id, attr) {
