891d1e21135872193f901e1592284c2343f5bd75
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-10-21 16:30:01 -0700

    simplify and expand demo loading

--- api/controllers/DemoController.js
+++ api/controllers/DemoController.js
@@ -21,11 +21,29 @@ module.exports = {
 
      
         var demoClinic = 1;
-        var demoUser = 'demoNurse';
+        var demoUser = 'DemoPatient';
+
+        if (!req.session.param) { req.session.param = {} }
 
         User.findOne( { name : demoUser} )
         .then ( function ( userData ) {
-            res.send({"DEMO Patient Data: " : userData});
+
+            console.log(JSON.stringify(userData));
+            req.session.param['User'] = userData;
+
+            var user_id = userData.id;
+
+            Patient.load( user_id, function (err, data) {
+                if (err) { res.send( { "Error" : "Error loading patient data", message : error }) }
+                req.session.param['patient'] = data.patient;
+                req.session.param['treatments'] = data.treatments;
+                req.session.param['schedule'] = data.schedule;
+                req.session.param['protectionMap'] = data.protectionMap;
+
+                console.log("**** Patient Data ***** " + JSON.stringify(data));
+                res.render('user/Patient', req.session.param);
+            });
+ 
         })
         .catch ( function (error) {
             res.send( { "Error" : "error loading user" });
@@ -44,36 +62,91 @@ module.exports = {
         .then ( function ( userData ) {
    
             req.session.param['User'] = userData;
- 
-            Clinic.load( {'clinic_id' : demoClinic, include : { staff: true, appointments : true} }, function (err, clinicData) {         
-                if (err) {
-                  console.log('no results');
-                  return res.send('');
+               
+            var payload = { id: userData.id, access: 'Demo' };
+            var token = null;
+            
+            console.log("payload: " + JSON.stringify(payload));
+
+            jwToken.issueToken(payload, function(err, result) {
+                if (err) { console.log("Error: " + err) }
+                else { 
+                    req.token = result;
+                    req.session.param['token'] = result;
+
+                    Clinic.load( {'clinic_id' : demoClinic, include : { staff: true, appointments : true} }, function (err, clinicData) {         
+                        if (err) {
+                          console.log('no results');
+                          return res.send('');
+                        }
+
+                        console.log("loaded data...");
+
+                        var page = { 
+                            item_Class : 'patient',
+                            search_title : "Search for Patients using any of fields below",
+                            add_to_scope : true
+                        };
+
+                        req.session.param['page'] = page;    
+                        req.session.param['clinic'] = clinicData;
+
+                        console.log("Page Input: " + JSON.stringify(req.session.param));
+                        // res.send({"DEMO Clinic Data: " : req.session.param});
+                        return res.render('clinic/Clinic', req.session.param);
+                    });
                 }
-
-                console.log("loaded data...");
-
-                var page = { 
-                    item_Class : 'patient',
-                    search_title : "Search for Patients using any of fields below",
-                    add_to_scope : true
-                };
-
-                req.session.param['page'] = page;    
-                req.session.param['clinic'] = clinicData;
-
-                console.log("PARAMS: " + JSON.stringify(req.session.param));
-                // res.send({"DEMO Clinic Data: " : req.session.param});
-                return res.render('clinic/Clinic', req.session.param);
             });
         });
 
-    },
+    },                                                                                                                                                                                                                                                                                                                                                  
 
     appointment : function (req, res) {
         console.log("Run Appointment Demo");
 
-        res.send({"DEMO" : "Appointment"});
+       var demoAppointment = 1;
+        var demoUser = 'demoNurse';
+        var region   = 1;
+
+        if (!req.session.param) { req.session.param = {} }
+
+        User.findOne( { name : demoUser} )
+        .then ( function ( userData ) {
+   
+            req.session.param['User'] = userData;
+               
+            var payload = { id: userData.id, access: 'Demo'};
+            var token = null;
+            
+            console.log("payload: " + JSON.stringify(payload));
+
+            jwToken.issueToken(payload, function(err, result) {
+                if (err) { return res.send({'Error' : err }) }
+                req.token = result;
+                req.session.param['token'] = result;
+
+                Appointment.loadAppointmentPageData( {appointment: demoAppointment}, function (err, data) {
+                    if (err) { return res.send( { 'Error' : err }) }
+
+                    console.log("*** set params:" + JSON.stringify(data));
+                    req.session.param['page'] = data.page;
+                    req.session.param['appointment'] = data.appointment;
+                    req.session.param['treatments'] = data.treatments;
+                    req.session.param['schedule'] = data.schedule;
+                    req.session.param['protectionMap'] = data.protectionMap;
+
+                    return res.render("appointment/Appointment", req.session.param);
+                });
+
+            });
+        });
+    },
+
+    research : function (req, res) {
+        console.log('Research Demo');
+
+        return res.render("research/Research", req.session.param);
     }
 };
 
+ 
