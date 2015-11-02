1b78402205fe6b9d71f0775a6a030584ca3d4388
Ran Guin <rguin@limsdev06.bcgsc.ca>
2015-11-02 14:22:54 -0800

    use jsonwebtoken rather than machinepack

--- api/services/jwToken.js
+++ api/services/jwToken.js
@@ -1,50 +1,18 @@
-var jwt = require('machinepack-jwt');
-var jwtAuth = require('machinepack-jwtauth');
+var jwt = require('jsonwebtoken');
 
 var tokenSecret = process.env.TOKEN_SECRET || 'defaultSecret';
 
 module.exports.issueToken = function(payload, callback) {
-    console.log("issue token for: " + JSON.stringify(payload));
+    console.log("Issue token for: " + JSON.stringify(payload));
 
-    var token = jwt.encode({
-        secret: tokenSecret,
-        payload: payload, 
-        algorithm: 'HS256',
-        expires: 43200,
-    }).exec({
-        error: function(err) {
-            callback("error encoding...");
-        },
-        success: function(result) {
-            console.log('Result: ' + result);
-            callback(null, result);
-        },
-    });
-}
+    var options = { 
+        expiredInMinutes : 1
+    };
+
+    return jwt.sign(payload, tokenSecret, options, callback);
+};
 
 module.exports.verifyToken = function(token, callback) {
-    jwtAuth.verifyFromHeader({
-        header: 'Authorization',
-        secret: tokenSecret,
-        schema: '*',
-        headerPrefix: 'Bearer ',
-        algorith: 'HS256',
-    }).exec({
-        error: function(err) {
-            console.log("jwt error" + err);
-            return callback("jwt error: " + err);
-        },
-        nullHeader: function() {
-            console.log("no token in header");
-            return callback("no token: " + err);
-        },
-        success: function() {
-           return callback(null, jwt.decode(token, tokenSecret));  
-        },
-    });
+    return jwt.verify(token, tokenSecret, {}, callback);
+};
 
-    function callback(err, result) {
-        if (err) { return console.log("Callback error: " + err) }
-        else { return result }
-    }
-}
